550a5f718be8ff9e52a35f2c5f400aa5e11bcc42
13th-Sammy <213174535+13th-Sammy@users.noreply.github.com>
2025-07-05 20:38:05 +0530

    Update

--- game.js
+++ game.js
@@ -1,5 +1,29 @@
 const canvas=document.getElementById("Game");
 const ctx=canvas.getContext("2d");
 
-ctx.fillStyle="red";
-ctx.fillRect(100, 100, 100, 100);
+let birdY=100;
+let birdVelocity=0;
+const gravity=0.5;
+
+const birdImg=new Image();
+birdImg.src="assets/bird.png";
+birdImg.onload=function() {
+    gameLoop();
+}
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    birdVelocity=birdVelocity+gravity;
+    birdY=birdY+birdVelocity;
+
+    ctx.drawImage(birdImg, 100, birdY, 40, 40);
+
+    requestAnimationFrame(gameLoop);
+}
+
+document.addEventListener("keydown", function(e) {
+    if(e.code === "Space") {
+        birdVelocity=-8;
+    }
+})
