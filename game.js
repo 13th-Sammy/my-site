3aac88ea7d354cf5164d01e9576466e784f4df4a
13th-Sammy <213174535+13th-Sammy@users.noreply.github.com>
2025-07-05 22:20:24 +0530

    Update

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 const canvas=document.getElementById("Game");
 const ctx=canvas.getContext("2d");
 
+let gameOver=false;
+
 let pipeGap=120;
 const pipeCount=2;
 const pipeSpacing=450;
@@ -27,12 +29,50 @@ const gravity=0.5;
 
 const birdImg=new Image();
 birdImg.src="assets/bird.png";
-birdImg.onload=function() {
+if(birdImg.complete) {
     gameLoop();
+} 
+else {
+    birdImg.onload = function() {
+        gameLoop();
+    };
 }
 
 function flap() {
     birdVelocity=-8;
+}
+
+function checkCollision(pipe) {
+    const birdX=100;
+    const birdWidth=40;
+    const birdHeight=40;
+
+    const pipeBottomY=pipe.topHeight+pipeGap;
+
+    if(birdY < 0 || birdY+birdHeight > canvas.height) {
+        return true;
+    }
+
+    if(birdX+birdWidth > pipe.x && birdX < pipe.x+pipeWidth &&
+       (birdY < pipe.topHeight || birdY+birdHeight > pipeBottomY)) {
+        return true;
+    }
+    
+    return false;
+}
+
+function restartGame() {
+    birdY=100;
+    birdVelocity=0;
+    pipes=[];
+    for(let i=0; i<pipeCount; i++) {
+        const topHeight=generatePipeHeight();
+        pipes.push({
+            x: canvas.width+i*pipeSpacing,
+            topHeight:topHeight
+        });
+    }
+    gameOver=false;
 }
 
 function gameLoop() {
@@ -59,17 +99,40 @@ function gameLoop() {
 
         const pipeBottomY=pipe.topHeight+pipeGap;
         ctx.fillRect(pipe.x, pipeBottomY, pipeWidth, canvas.height-pipeBottomY);
+
+        if(checkCollision(pipe)) {
+            gameOver=true;
+        }
+    }
+
+    if(gameOver) {
+        ctx.font="30px Arial";
+        ctx.fillStyle="red";
+        ctx.textAlign = "center";
+        ctx.fillText("Game Over", canvas.width / 2, canvas.height / 2);
+        return;
     }
 
     requestAnimationFrame(gameLoop);
 }
 
 document.addEventListener("keydown", (e) => {
-    if(e.code === "Space") {
+    if(gameOver) {
+        if(e.code === "Space") {
+            restartGame();
+        }
+    }
+    else if(e.code === "Space") {
         flap();
     }
 });
 
 document.addEventListener("touchstart", (e) => {
-    flap();
-});
+    e.preventDefault();
+    if(gameOver) {
+        restartGame();
+    }
+    else {
+        flap();
+    }
+}, {passive: false});
