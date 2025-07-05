bbcbea9845f6a096829def5929c408150d2f6a24
13th-Sammy <213174535+13th-Sammy@users.noreply.github.com>
2025-07-05 21:04:22 +0530

    Update

--- game.js
+++ game.js
@@ -5,6 +5,11 @@ let birdY=100;
 let birdVelocity=0;
 const gravity=0.5;
 
+const pipeWidth=60;
+let pipeX=canvas.width;
+let pipeGap=100;
+let pipeTopHeight=Math.floor(Math.random()*canvas.height-pipeGap-100)+50;
+
 const birdImg=new Image();
 birdImg.src="assets/bird.png";
 birdImg.onload=function() {
@@ -18,6 +23,18 @@ function gameLoop() {
     birdY=birdY+birdVelocity;
 
     ctx.drawImage(birdImg, 100, birdY, 40, 40);
+
+    pipeX=pipeX-2;
+    if(pipeX+pipeWidth<0) {
+        pipeX=canvas.width;
+        pipeTopHeight=Math.floor(Math.random()*canvas.height-pipeGap-100)+50;
+    }
+
+    ctx.fillStyle="green";
+    ctx.fillRect(pipeX, 0, pipeWidth, pipeTopHeight);
+
+    const pipeBottomY=pipeTopHeight+pipeGap;
+    ctx.fillRect(pipeX, pipeBottomY, pipeWidth, canvas.height-pipeBottomY);
 
     requestAnimationFrame(gameLoop);
 }
